97bad079feb54b53e61a1b19b4177904fde21930
yemeal <emelyalex@bk.ru>
2026-03-16 17:42:10 +0700

    feat: Implement Singleton Pattern

--- RPM_Lab4/Program.cs
+++ RPM_Lab4/Program.cs
@@ -1,9 +1,11 @@
 ﻿using RPM_Lab4.Models;
 using RPM_Lab4.Factories;
+using RPM_Lab4.Registries;
 
 
 #region Паттерн "Строитель"
 Console.WriteLine("=== Builder Pattern ===\n");
+
 ComputerConstructor computerConstructor = new();
 
 OfficeComputerFactory officeComputerFactory = new();
@@ -25,6 +27,7 @@ Console.WriteLine(homeComputer.Display());
 
 #region Паттерн "Прототип"
 Console.WriteLine("\n\n=== Prototype Pattern ===\n");
+
 Console.WriteLine("Home Computer:\n");
 Console.WriteLine(homeComputer.Display());
 Computer shallowClone = homeComputer.ShallowCopy();
@@ -43,4 +46,21 @@ Console.WriteLine(deepClone.Display());
 
 
 #region Паттерн "Синглтон"
+Console.WriteLine("\n\n=== Singleton Pattern ===\n");
+
+PrototypeRegistry prototypeRegistry = PrototypeRegistry.Instance;
+
+Computer gamingComputerPrototype1 = prototypeRegistry.GetPrototype("gaming");
+
+Console.WriteLine("\nPrototype 1:\n");
+Console.WriteLine(gamingComputerPrototype1.Display());
+Console.WriteLine("\nAdding more RAM to Prototype1...\n");
+gamingComputerPrototype1.RAM = 9999;
+Console.WriteLine("Creating Prototype2...\n");
+Computer gamingComputerPrototype2 = prototypeRegistry.GetPrototype("gaming");
+
+Console.WriteLine("\nPrototype 1 with more RAM:\n");
+Console.WriteLine(gamingComputerPrototype1.Display());
+Console.WriteLine("\nPrototype 2:\n");
+Console.WriteLine(gamingComputerPrototype2.Display());
 #endregion
---
+++ RPM_Lab4/Registries/PrototypeRegistry.cs
@@ -0,0 +1,50 @@
+﻿using RPM_Lab4.Models;
+using RPM_Lab4.Factories;
+
+namespace RPM_Lab4.Registries
+{
+    public class PrototypeRegistry
+    {
+        private readonly Dictionary<string, Computer> _prototypesDict = [];
+        private PrototypeRegistry () 
+        {
+            ComputerConstructor computerConstructor = new();
+
+            OfficeComputerFactory officeComputerFactory = new();
+            GamingComputerFactory gamingComputerFactory = new();
+            HomeComputerFactory homeComputerFactory = new();
+
+            Computer officeComputer = computerConstructor.Construct(officeComputerFactory);
+            Computer gamingComputer = computerConstructor.Construct(gamingComputerFactory);
+            Computer homeComputer = computerConstructor.Construct(homeComputerFactory);
+
+            _prototypesDict["office"] = officeComputer;
+            _prototypesDict["gaming"] = gamingComputer;
+            _prototypesDict["home"] = homeComputer;
+        }
+
+        private static volatile PrototypeRegistry? _instance = null;
+        private static readonly Lock _lock = new();
+        public static PrototypeRegistry Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock) 
+                    { 
+                        _instance ??= new PrototypeRegistry ();
+                    }
+                }
+                return _instance;
+            }
+
+        }
+
+        public Computer GetPrototype(string key)
+        {
+            return _prototypesDict[key].DeepCopy();
+        }
+        
+    }
+}
