1b5412cb9d0d8b2695d3114e2a598d86ea43c35b
yemeal <emelyalex@bk.ru>
2026-03-19 02:56:23 +0700

    feat: Implement Proxy pattern

---
+++ RPM_Lab6/Proxy/HighResImage.cs
@@ -0,0 +1,30 @@
+﻿using RPM_Lab6.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab6.Proxy
+{
+    public class HighResImage : IGraphic
+    {
+        private string _filePath;
+
+        public HighResImage(string filePath)
+        {
+            _filePath = filePath;
+            LoadFromDisk();
+        }
+
+        private void LoadFromDisk()
+        {
+            Console.WriteLine($"[Proxy - Загрузка] Чтение тяжелого изображения с диска: {_filePath}...");
+            Thread.Sleep(1000); // Имитация долгой загрузки
+            Console.WriteLine($"[Proxy - Завершено] Изображение {_filePath} загружено в память.");
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"Отрисовка изображения высокого разрешения: {_filePath}");
+        }
+    }
+}
---
+++ RPM_Lab6/Proxy/ImageProxy.cs
@@ -0,0 +1,28 @@
+﻿using RPM_Lab6.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab6.Proxy
+{
+    public class ImageProxy : IGraphic
+    {
+        private HighResImage _realImage;
+        private string _filePath;
+
+        public ImageProxy(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Draw()
+        {
+            // Ленивая инициализация (Lazy Initialization)
+            if (_realImage == null)
+            {
+                _realImage = new HighResImage(_filePath);
+            }
+            _realImage.Draw();
+        }
+    }
+}
