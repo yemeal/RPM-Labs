32b58f82f72824085319cfbea4d6a9274c68df3c
yemeal <emelyalex@bk.ru>
2026-03-19 02:53:28 +0700

    feat: Implement Flyweight pattern

---
+++ RPM_Lab6/Flyweight/CharacterFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab6.Flyweight
+{
+    public class CharacterFactory
+    {
+        private Dictionary<string, CharacterFormat> _characters = new Dictionary<string, CharacterFormat>();
+
+        public CharacterFormat GetCharacter(char symbol, string font, int size)
+        {
+            string key = $"{symbol}_{font}_{size}";
+            if (!_characters.ContainsKey(key))
+            {
+                _characters[key] = new CharacterFormat(symbol, font, size);
+                Console.WriteLine($"[Flyweight] Создан новый объект для '{symbol}'");
+            }
+            else
+            {
+                Console.WriteLine($"[Flyweight] Взят из кэша объект для '{symbol}'");
+            }
+            return _characters[key];
+        }
+    }
+}
