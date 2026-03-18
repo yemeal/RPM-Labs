c569c8e4ae65d55de72c7b6073b7a87025df1e48
yemeal <emelyalex@bk.ru>
2026-03-19 02:57:54 +0700

    feat: Implement Bridge pattern

---
+++ RPM_Lab6/Bridge/IRenderEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab6.Bridge
+{
+    public interface IRenderEngine
+    {
+        void RenderCircle(float radius);
+        void RenderRectangle(float width, float height);
+    }
+}
---
+++ RPM_Lab6/Bridge/Shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab6.Bridge
+{
+    internal class Shape
+    {
+    }
+}
