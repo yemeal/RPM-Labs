c5c05670e2685e29347df6defbd50d276be1e46f
yemeal <emelyalex@bk.ru>
2026-03-20 02:57:33 +0700

    feat: Implement Adapter pattern

---
+++ RPM_Lab5/Adapter/IFileSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab5.Adapter
+{
+    public interface IFileSystem
+    {
+        List<string> ListItems(string path);
+        byte[] ReadFile(string path);
+        void WriteFile(string path, byte[] data);
+        void DeleteItem(string path);
+    }
+}
