395182a191ac453cdddd252d0ebc6ee4c4c803ca
yemeal <emelyalex@bk.ru>
2026-03-20 02:55:38 +0700

    feat: Implement Composite pattern

---
+++ RPM_Lab5/Composite/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab5.Composite
+{
+    public class File(string name, long size) : FileSystemItem(name)
+    {
+        private long _size = size;
+
+        public override long GetSize()
+        {
+            return _size;
+        }
+    }
+}
---
+++ RPM_Lab5/Composite/FileSystemItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPM_Lab5.Composite
+{
+    public abstract class FileSystemItem(string name)
+    {
+        public string Name { get; set; } = name;
+
+        public abstract long GetSize();
+
+        public virtual void Add(FileSystemItem item)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public virtual void Remove(FileSystemItem item)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public virtual FileSystemItem GetChild(int index)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
