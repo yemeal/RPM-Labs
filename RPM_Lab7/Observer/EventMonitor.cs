c5ec86e00e2ce90e4ab7b54b5a0b140d7e6e763c
yemeal <emelyalex@bk.ru>
2026-03-25 02:43:06 +0700

    feat: Implement Observer pattern

---
+++ RPM_Lab7/Observer/EventMonitor.cs
@@ -0,0 +1,23 @@
+﻿namespace RPM_Lab7.Observer;
+
+public delegate void MetricEventHandler(MetricEventArgs e);
+
+/// <summary>
+/// Субъект (Subject). Вместо интерфейса ISubject и методов Attach/Detach
+/// использует стандартное событие .NET.
+/// </summary>
+public class EventMonitor
+{
+    // Событие (Event) — это ключевой элемент паттерна Observer в C#
+    public event MetricEventHandler? OnMetricExceeded;
+
+    public void CheckMetric(string metricName, double value, double threshold)
+    {
+        Console.WriteLine($"[Monitor]: Checking {metricName} ({value} vs {threshold})");
+        if (!(value > threshold)) return;
+        
+        MetricData eventData = new(metricName, value, threshold, DateTime.UtcNow);
+        OnMetricExceeded?.Invoke(new MetricEventArgs(eventType: metricName +
+                                                                "_Exceeded", data: eventData));
+    }
+}
---
+++ RPM_Lab7/Observer/MetricEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace RPM_Lab7.Observer;
+
+public class MetricEventArgs(string eventType, MetricData data) : EventArgs
+{
+    public string EventType { get; } = eventType ?? throw new ArgumentNullException(nameof(eventType));
+    
+    public MetricData Data { get; } = data ?? throw new ArgumentNullException(nameof(data));
+}
